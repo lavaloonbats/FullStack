43ee9197de2988a7d5b3c6ec61f6f854ec41ab79
OSX <osx@OSXs-MacBook-Pro.local>
2026-05-23 09:58:33 +1000

    fullstack project setup

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/message")
+      .then((res) => setMessage(res.data.message))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div>
+      <h1>Full Stack DevOps Project 🚀</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
